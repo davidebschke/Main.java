6a8ca1c5451cffab62ce5b5f758a11c2c5df52e1
davidebschke <Davidebschke8@gmail.com>
2022-06-30 11:53:50 +0200

    Testing_Beginn

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static int numberlog(int number)
+    {
+        number=1;
+        return number;
+    }
+
+    public static int addieren(int zahl1,int zahl2){
+
+        int zahl3=zahl1+zahl2;
+
+        return zahl3;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    void Number1() {
+        //given
+        int number = 1;
+        //when
+        int actual = Main.numberlog(number);
+        //then
+        int expected = 1;
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void Addierencontrol() {
+
+        //given                                     // Welche variablen werden benötigt
+
+        int zahl1 = 2;
+        int zahl2 = 2;
+
+        //when                                      // Wenn er was ausführt
+        int actual = Main.addieren(zahl1, zahl2);
+
+        //then                                      // Erwarten wir folgendes Ergebnis
+        int expected = 4;
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
